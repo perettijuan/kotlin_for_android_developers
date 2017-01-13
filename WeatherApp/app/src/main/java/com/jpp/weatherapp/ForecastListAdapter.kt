c06e6044a659ded67f31a3187b9e192068f6de35
Juan Pablo Peretti <jpperetti@arcba00014166.ml.com>
2017-01-13 17:04:45 -0300

    Chapter done

---
+++ WeatherApp/app/src/main/java/com/jpp/weatherapp/ForecastListAdapter.kt
@@ -0,0 +1,50 @@
+package com.jpp.weatherapp
+
+import android.support.v7.widget.RecyclerView
+import android.view.ViewGroup
+import android.widget.TextView
+
+/**
+ * Class that extends RecyclerView.Adapter and provides functionallity to show
+ * the list of weather conditions.
+ *
+ * It receives a List of Strings as constructor parameters.
+ *
+ * Created by jpperetti on 13/1/17.
+ */
+class ForecastListAdapter(val items: List<String>) : RecyclerView.Adapter<ForecastListAdapter.ViewHolder>() {
+
+
+    /**
+     * Overrides the function onCreateViewHolder. Note that ViewGroup might be null (indicated
+     * by the ? after the parameter type). So, check nullity in Kotlin is used inside the
+     * method's definition (!!).
+     */
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): ViewHolder {
+        return ViewHolder(TextView(parent!!.context))
+    }
+
+
+    /**
+     * Overrides the function onBindViewHolder. Note that the holder parameter might be null,
+     * so I'm using !! again.
+     */
+    override fun onBindViewHolder(holder: ViewHolder?, position: Int) {
+        holder!!.textView.text = items[position]
+    }
+
+
+    /**
+     * Overrides the function getItemCount. Note that I'm not using the brackets here, since
+     * Kotlin allows the no-brackets syntax if the function can be calculated in a single line.
+     */
+    override fun getItemCount(): Int = items.size
+
+    /**
+     * Inner class definition. Note that it extends RecyclerView.ViewHolder and, since this base class
+     * declare a single argument constructor, I'm indicating that textView (the parameter
+     * in the constructor of this inner class) is the parameter passed to that constructor.
+     */
+    class ViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView)
+
+}
--- WeatherApp/app/src/main/java/com/jpp/weatherapp/MainActivity.kt
+++ WeatherApp/app/src/main/java/com/jpp/weatherapp/MainActivity.kt
@@ -2,14 +2,29 @@ package com.jpp.weatherapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 
 import kotlinx.android.synthetic.main.activity_main.*;
 
 class MainActivity : AppCompatActivity() {
 
+    private val items = listOf(
+            "Aaaaaaaaaaaa",
+            "Bbbbbbbbbbbb",
+            "Cccccccccccc",
+            "Dddddddddddd",
+            "Eeeeeeeeeeee",
+            "Ffffffffffff",
+            "Gggggggggggg"
+    )
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        message.text = "Hello Baby Kotlin"
+        val forecastList = findViewById(R.id.forecast_list) as RecyclerView
+        forecastList.layoutManager = LinearLayoutManager(this)
+        forecastList.adapter = ForecastListAdapter(items)
     }
 }
