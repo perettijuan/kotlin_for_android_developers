be80f73f08a4f9790fb2899f8bb99044a0b84a16
Juan Pablo Peretti <jpperetti@arcba00014166.ml.com>
2017-01-16 17:00:07 -0300

    Anko to find view by id

--- WeatherApp/app/src/main/java/com/jpp/weatherapp/MainActivity.kt
+++ WeatherApp/app/src/main/java/com/jpp/weatherapp/MainActivity.kt
@@ -1,11 +1,10 @@
 package com.jpp.weatherapp
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
-
-import kotlinx.android.synthetic.main.activity_main.*;
+import org.jetbrains.anko.find
 
 class MainActivity : AppCompatActivity() {
 
@@ -23,7 +22,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val forecastList = findViewById(R.id.forecast_list) as RecyclerView
+        val forecastList: RecyclerView = find(R.id.forecast_list)
         forecastList.layoutManager = LinearLayoutManager(this)
         forecastList.adapter = ForecastListAdapter(items)
     }
