1694ec549c0ee6534e0283e6b04c78d0e9bad1f2
Juan Pablo Peretti <jpperetti@arcba00014166.ml.com>
2017-01-20 15:52:29 -0300

    Chapter Done

--- WeatherApp/app/src/main/java/com/jpp/weatherapp/ui/ForecastListAdapter.kt
+++ WeatherApp/app/src/main/java/com/jpp/weatherapp/ui/ForecastListAdapter.kt
@@ -4,14 +4,14 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import com.jpp.weatherapp.R
 import com.jpp.weatherapp.domain.model.Forecast
 import com.jpp.weatherapp.domain.model.ForecastList
 import com.jpp.weatherapp.ui.utils.ctx
 import com.squareup.picasso.Picasso
-import org.jetbrains.anko.find
+
+
+import kotlinx.android.synthetic.main.item_forecast.view.*
 
 /**
  * Class that extends RecyclerView.Adapter and provides functionallity to show
@@ -41,7 +41,7 @@ class ForecastListAdapter(val weekForecast: ForecastList, val itemClick: (Foreca
      * so I'm using !! again.
      */
     override fun onBindViewHolder(holder: ViewHolder?, position: Int) {
-       holder!!.bindForecast(weekForecast[position])
+        holder!!.bindForecast(weekForecast[position])
     }
 
 
@@ -56,29 +56,18 @@ class ForecastListAdapter(val weekForecast: ForecastList, val itemClick: (Foreca
      * declare a single argument constructor, I'm indicating that textView (the parameter
      * in the constructor of this inner class) is the parameter passed to that constructor.
      */
-    class ViewHolder(val view: View, val itemClick: (Forecast) -> Unit ) : RecyclerView.ViewHolder(view) {
+    class ViewHolder(view: View, val itemClick: (Forecast) -> Unit) : RecyclerView.ViewHolder(view) {
 
-        private val iconView: ImageView
-        private val dateView: TextView
-        private val descriptionView: TextView
-        private val maxTemperatureView: TextView
-        private val minTemperatureView: TextView
-
-        init {
-            iconView = view.find(R.id.icon)
-            dateView = view.find(R.id.date)
-            descriptionView = view.find(R.id.description)
-            maxTemperatureView = view.find(R.id.maxTemperature)
-            minTemperatureView = view.find(R.id.minTemperature)
-        }
 
         fun bindForecast(forecast: Forecast) {
             with(forecast) {
-                Picasso.with(itemView.ctx).load(iconUrl).into(iconView)
-                dateView.text = date
-                descriptionView.text = description
-                maxTemperatureView.text = "${high}"
-                minTemperatureView.text = "${low}"
+
+                // Thanks to kotlin android extentions we don't need find!
+                Picasso.with(itemView.ctx).load(iconUrl).into(itemView.icon)
+                itemView.date.text = date
+                itemView.description.text = description
+                itemView.maxTemperature.text = "${high}"
+                itemView.minTemperature.text = "${low}"
 
                 // assign the item click listener -> operator overload invoke allows me to just call itemClick()
                 itemView.setOnClickListener { itemClick(this) }
--- WeatherApp/app/src/main/java/com/jpp/weatherapp/ui/MainActivity.kt
+++ WeatherApp/app/src/main/java/com/jpp/weatherapp/ui/MainActivity.kt
@@ -1,13 +1,13 @@
 package com.jpp.weatherapp.ui
 
+// synthetic import -> allows creating views without findView() methods!
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
 import com.jpp.weatherapp.R
 import com.jpp.weatherapp.domain.command.RequestForecastCommand
+import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.async
-import org.jetbrains.anko.find
 import org.jetbrains.anko.toast
 import org.jetbrains.anko.uiThread
 
@@ -17,7 +17,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val forecastList: RecyclerView = find(R.id.forecast_list)
         forecastList.layoutManager = LinearLayoutManager(this)
 
 
