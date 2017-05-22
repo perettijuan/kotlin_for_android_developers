49e2a111ea86f07477f7e617a3e6c2e2208ca703
Juan Peretti <jpp@Juans-MacBook-Pro.local>
2017-05-22 11:09:53 -0300

    Clear repo

--- WeatherApp/app/src/main/java/com/jpp/weatherapp/ui/OnItemClickListener.kt
+++
@@ -1,9 +0,0 @@
-package com.jpp.weatherapp.ui
-
-import com.jpp.weatherapp.domain.model.Forecast
-
-interface OnItemClickListener {
-
-    // override the operator invoke
-    operator fun invoke(forecast: Forecast)
-}
--- WeatherApp/app/src/main/java/com/jpp/weatherapp/ui/utils/ViewExtrentions.kt
+++
@@ -1,8 +0,0 @@
-package com.jpp.weatherapp.ui.utils
-
-import android.content.Context
-import android.view.View
-
-// extention funtion to retrieve the context from a View.
-val View.ctx: Context
-    get() = context
