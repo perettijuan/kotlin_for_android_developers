374852a33e666c885c8ab8f59855318085d6f3b2
Juan Peretti <peretti.juan@gmail.com>
2017-06-09 12:16:19 -0300

    Chapter 23

--- WeatherApp/app/src/main/java/com/jpp/kotlin/weatherapp/ui/activities/MainActivity.kt
+++ WeatherApp/app/src/main/java/com/jpp/kotlin/weatherapp/ui/activities/MainActivity.kt
@@ -8,7 +8,7 @@ import com.jpp.kotlin.weatherapp.domain.commands.RequestForecastCommand
 import com.jpp.kotlin.weatherapp.ui.adapters.ForecastListAdapter
 import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.doAsync
-import org.jetbrains.anko.toast
+import org.jetbrains.anko.startActivity
 import org.jetbrains.anko.uiThread
 
 class MainActivity : AppCompatActivity() {
@@ -21,7 +21,11 @@ class MainActivity : AppCompatActivity() {
         doAsync {
             val result = RequestForecastCommand(94043).execute()
             uiThread {
-                forecastList.adapter = ForecastListAdapter(result, { toast(it.description) })
+                val adapter = ForecastListAdapter(result) {
+                    startActivity<DetailActivity>(DetailActivity.ID to it.id, DetailActivity.CITY_NAME to result.city)
+                }
+                forecastList.adapter = adapter
+                title = "${result.city} (${result.country})"
             }
         }
     }
