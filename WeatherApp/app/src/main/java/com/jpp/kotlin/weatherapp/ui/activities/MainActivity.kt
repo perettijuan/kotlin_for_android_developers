edf236de4438aca5f7c21a31b9a7fdba45c972ce
Juan Peretti <peretti.juan@gmail.com>
2017-06-14 20:32:02 -0300

    Chapter 24

--- WeatherApp/app/src/main/java/com/jpp/kotlin/weatherapp/ui/activities/MainActivity.kt
+++ WeatherApp/app/src/main/java/com/jpp/kotlin/weatherapp/ui/activities/MainActivity.kt
@@ -3,20 +3,31 @@ package com.jpp.kotlin.weatherapp.ui.activities
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.Toolbar
 import com.jpp.kotlin.weatherapp.R
 import com.jpp.kotlin.weatherapp.domain.commands.RequestForecastCommand
 import com.jpp.kotlin.weatherapp.ui.adapters.ForecastListAdapter
 import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.find
 import org.jetbrains.anko.startActivity
 import org.jetbrains.anko.uiThread
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), ToolbarManager {
+
+    override val toolbar by lazy { find<Toolbar>(R.id.toolbar) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // interface method!
+        initToolbar()
+
         forecastList.layoutManager = LinearLayoutManager(this)
+
+        // interface method!
+        attachToScroll(forecastList)
 
         doAsync {
             val result = RequestForecastCommand(94043).execute()
